0098519fd1dcec957516c8a4b08aa30672140154
Jose Jesus <s21120206@alumnos.itsur.edu.mx>
2024-10-07 21:40:44 -0600

    Se agrego el remember para guardar el count

--- BasicStateCodelab2/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab2/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,5 +1,6 @@
 package com.example.basicstatecodelab
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -7,11 +8,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
+
         Text("You've had $count glasses.")
         Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
