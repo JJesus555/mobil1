453ce3e792cc7c7540393a4b8758e294ec86574d
JJesus555 <s21120206@alumnos.itsur.edu.mx>
2024-10-07 22:51:20 -0600

    Se termino la practica

--- BasicStateCodelab2/app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
+++ BasicStateCodelab2/app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.runtime.remember
 
 @Composable
 fun WellnessTasksList(
