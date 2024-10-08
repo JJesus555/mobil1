881489f98bf16417370ada70a1e7ffd8033d49f1
Jose Jesus <s21120206@alumnos.itsur.edu.mx>
2024-10-07 22:23:29 -0600

    Se realizaron los cambios finales y se agregaron archivos

--- BasicStateCodelab2/app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
+++ BasicStateCodelab2/app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
@@ -17,15 +17,27 @@ import androidx.compose.runtime.saveable.rememberSaveable
 
 @Composable
 fun WellnessTaskItem(
-    taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier
+    taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    var checkedState by rememberSaveable { mutableStateOf(false) }
-
-    WellnessTaskItem(
-        taskName = taskName,
-        checked = checkedState,
-        onCheckedChange = { newValue -> checkedState = newValue },
-        onClose = onClose,
-        modifier = modifier,
-    )
+    Row(
+        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
+            text = taskName
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
 }
